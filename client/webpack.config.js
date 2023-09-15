b509f6c3b918e63ffc7d3270064253cc2480608f
Adrienne Kecskes <akecs619@yahoo.com>
2023-09-15 00:01:54 -0500

    add service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
@@ -21,6 +21,7 @@ module.exports = () => {
     },
     plugins: [
       new MiniCssExtractPlugin(),
+      new WorkboxPlugin.GenerateSW()
       
     ],
 
