1874fb525f61785e816bf93e66fa1a0dbe27bf8b
Adrienne Kecskes <akecs619@yahoo.com>
2023-09-15 16:04:21 -0500

    database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,6 +23,14 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+  console.error("getDb not implemented");
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const content = await store.getAll();
+  await tx.done;
+  return content;
 
+}
 initdb();
